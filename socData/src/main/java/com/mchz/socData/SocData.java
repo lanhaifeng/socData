cc02ff224fb7c370979dcc218e553fb2bf05b9a1
lanhaifeng <lanhaifeng@mchz.com.cn>
2019-10-17 09:30:14 +0800

    搭建基础生成soc数据项目

---
+++ socData/src/main/java/com/mchz/socData/SocData.java
@@ -0,0 +1,16 @@
+package com.mchz.socData;
+
+/**
+ * generateData
+ * 2019/10/17 9:29
+ * main函数入口类
+ *
+ * @author lanhaifeng
+ * @since
+ **/
+public class SocData {
+
+	public static void main(String[] args) {
+
+	}
+}
