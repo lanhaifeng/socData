81a4e3b53c36e797d0988ded009bf80fd2a3300d
lanhaifeng <lanhaifeng@mchz.com.cn>
2019-10-18 11:12:10 +0800

    添加后台输出

--- socData/src/main/java/com/mchz/socData/SocData.java
+++ socData/src/main/java/com/mchz/socData/SocData.java
@@ -7,14 +7,12 @@ import com.mchz.socData.util.CreateFileUtil;
 import com.mchz.socData.util.GenerateDataUtil;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -48,7 +46,7 @@ public class SocData {
 
             CreateFileUtil.createJsonFile(json, fileUrl, fileName);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("生成json数据文件错误：" + ExceptionUtils.getFullStackTrace(e));
         }
     }
 
@@ -69,7 +67,7 @@ public class SocData {
             }
             return array;
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("生成json数据错误：" + ExceptionUtils.getFullStackTrace(e));
         }
         return null;
     }
@@ -177,7 +175,7 @@ public class SocData {
             }
             br.close();
         } catch (IOException e) {
-            e.getStackTrace();
+            System.out.println("读取json模板文件错误：" + ExceptionUtils.getFullStackTrace(e));
         }
         return strbuffer.toString();
     }
